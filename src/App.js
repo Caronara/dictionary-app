7ae8cf0232cd3ac09cd711b39e56050056385056
Caronara <97363450+Caronara@users.noreply.github.com>
2022-04-27 14:38:56 +0200

    Added the search form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
